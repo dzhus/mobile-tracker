1fa72f55caf96be23d93f0fdc6732e3c6e1086b9
Dmitry Dzhus <dima@sphinx.net.ru>
2009-08-21 19:29:46 +0400

    Simple location tracking J2ME application for SonyEricsson phones.

---
+++ src/MobileTracker.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+
+public class MobileTracker
+    extends MIDlet
+    implements CommandListener {
+
+    private Form form;
+    private String lac;
+    private String cellid;
+
+    /**
+     * Time interval between location updates, in ms.
+     */
+    private int updateTimeout = 5000;
+
+    private StringItem timeItem = new StringItem("Last update:", null);
+    private StringItem locationItem = new StringItem("Location: ", null);
+
+    /**
+     * TextBox which contains locations history over the last time.
+     */
+    private TextField historyBox = new TextField("History:", null, 1000, TextField.UNEDITABLE);
+
+    private Timer timer = new Timer();
+
+    /**
+     * Create a form with elements.
+     */
+    public MobileTracker() {
+        setupTimer();
+        form = new Form("Mobile Tracker");
+        form.append(timeItem);
+        form.append(locationItem);
+        
+        form.append(historyBox);
+
+        timeItem.setLayout(Item.LAYOUT_NEWLINE_AFTER);
+        locationItem.setLayout(Item.LAYOUT_NEWLINE_AFTER);
+
+        form.addCommand(new Command("Exit", Command.EXIT, 0));
+        form.setCommandListener(this);
+    }
+
+    /**
+     * Update location, refresh form and log.
+     */
+    private TimerTask updateTask = new TimerTask() {
+            public void run() {
+                logLocation();
+                updateLocation();
+                updateForm();
+            }
+        };
+
+    private void updateLocation() {
+        cellid = System.getProperty("com.sonyericsson.net.cellid");
+        lac = System.getProperty("com.sonyericsson.net.lac");
+    }
+
+    private String makeLocationMark(String cellid, String lac) {
+        return cellid + "@" + lac;
+    }
+
+    private void updateForm() {
+        locationItem.setText(makeLocationMark(cellid, lac));
+        timeItem.setText(new Date().toString());
+    }
+
+    private void logLocation() {
+        historyBox.insert(makeLocationMark(cellid, lac) + "\n", historyBox.size());
+    }
+
+    private void setupTimer() {
+        timer.schedule(updateTask, 0, updateTimeout);
+    }
+
+    public final void startApp() {
+        Display.getDisplay(this).setCurrent(form);
+    }
+
+    public final void pauseApp() {
+    }
+
+    public final void destroyApp(boolean unconditional) {
+    }
+
+    public final void commandAction(Command c, Displayable s) {
+        notifyDestroyed();
+    }
+}
